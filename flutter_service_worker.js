0cf685931f616ec3f3300a5f062c439ec948c94a
gongpengyang <gongpengyang@58.com>
2023-03-22 17:43:27 +0800

    1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "ed64f95f0f9b06a16ab56d5e7ccec062",
-"index.html": "3d3e8c9a89b3d3be79512ea15707aaf3",
-"/": "3d3e8c9a89b3d3be79512ea15707aaf3",
-"main.dart.js": "9ac964d1fc1ad38352b64a641bdb907e",
+"index.html": "16e4437ad53ee1fbff4fb196db06fd00",
+"/": "16e4437ad53ee1fbff4fb196db06fd00",
+"main.dart.js": "aa09c5e4ca88eaa762e6d1610138620a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
